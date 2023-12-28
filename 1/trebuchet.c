8011732b58164710085021eb422c02574f41957f
christopher7711 <christopherhirtler@gmail.com>
2023-12-28 07:00:20 +0100

    solution day1

--- 1/trebuchet.c
+++ 1/trebuchet.c
@@ -1,12 +1,28 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <ctype.h>
+#include <string.h>
 int getCalibrationValue(char * input,ssize_t length)
 {
-   printf("%s",input);
-   printf("%zu \n",length - 1); // -1 \n
+   char result[3];
+   result[0] = 'a';
+   //printf("%s",input);
+   //printf("%zu \n",length - 1); // -1 \n
+   for(int i=0;i < length ; i++){
+    //printf("%c",input[i]);
+    if(isdigit(input[i])){
+      if(result[0] == 'a')
+      {
+        result[0] = input[i];
+      }
+      result[1] = input[i]; 
+    }
+    result[2] =  '\n';
+  }
+  return atoi(result);
+  //puts(result);
+  //printf("%d",value);
 }
 
 
@@ -17,15 +33,17 @@ int main(void)
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
+    int Result = 0;
 
     fp = fopen("input", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     while ((read = getline(&line, &len, fp)) != -1) {
-	getCalibrationValue(line,read);
+	  Result = Result + getCalibrationValue(line,read);
+    printf("%d \n",Result);
     }
-
+    printf("%d",Result);
     fclose(fp);
     if (line)
         free(line);
